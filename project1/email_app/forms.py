e3b1145deb60e29eeabb0e9ff8c4555a7f5e27dc
Ankit809898 <ankitsomkuwar71@gmail.com>
2023-10-13 10:59:22 +0530

    initial commit

--- project1/email_app/forms.py
+++ project1/email_app/forms.py
@@ -5,6 +5,6 @@ from django import forms
 class EmailForm(forms.Form):
    email = forms.EmailField()
    subject = forms.CharField(max_length=1000)
-   attach = forms.FileField(widget=forms.FileInput(attrs={'multiple':True}))
+   attach = forms.FileField(widget=forms.FileInput(attrs={'multiple':False}))
    message = forms.CharField(widget=forms.Textarea)
 
